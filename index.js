49f2b74f74c1e302447ce9913e035c8ee97f08d9
Utsav4862 <utsavdholiya48@gmail.com>
2023-04-21 21:13:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+require("dotenv").config({ path: ".env" });
+require("./config");
+
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+const jwt = require("jsonwebtoken");
+const port = process.env.PORT || 5550;
+const bodyParser = require("body-parser");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+    parameterLimit: 10000,
+    limit: "100mb",
+  })
+);
+
+const routes = require("./Routes/main");
+app.use(cors());
+app.use(express.json());
+app.use(express.static("uploads"));
+app.use("/api", routes);
+const server = app.listen(port);
